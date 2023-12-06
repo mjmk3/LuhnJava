16c0852e7432fd569f2b744074dba82aaa482447
Mohamad J Makki <mohamad.j.makki@gmail.com>
2023-12-06 12:43:55 +0300

    Credit Card Validator with Luhn Algorithm

---
+++ src/test/java/app/luhn/luhn/LuhnApplicationTests.java
@@ -0,0 +1,13 @@
+package app.luhn.luhn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuhnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
